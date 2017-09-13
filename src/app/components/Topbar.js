bdd2f772a95f24e7825c0c8c6b77305a7d4cff04
Ori Shuchami <ori_s@ifat.com>
2017-09-13 14:48:37 +0300

    played a bit with foundation, no success. seems that foundation-sites does not work well with CSSModules.

--- src/app/components/Topbar.js
+++ src/app/components/Topbar.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react'
-import CSSModules from 'react-css-modules'
-import styles from './Topbar.scss'
+//import CSSModules from 'react-css-modules'
+//import styles from './Topbar.scss'
 import {translate} from 'react-polyglot'
 import {inject} from 'mobx-react'
 
+const req = require.context('common/style/icons/', false)
+const logoSrc = req('./logo.png')
+const navIconSrc = req('./nav_icon.svg')
+const userSrc = req('./user.svg')
+
 const navbar = [  {
-  title: 'search',
-  link: '/search'
+  title: 'subscriptions',
+  link: '/subscriptions'
 }, {
-  title: 'test',
-  link: '/test'
+  title: 'contactus',
+  link: '/contact'
+}, {
+  title: 'publish',
+  link: '/publish'
 }]
 
 @translate()
 @inject('translationsStore')
 @inject('routingStore')
-@CSSModules(styles)
+//@CSSModules(styles, { allowMultiple: true })
 export default class Topbar extends Component {
 
   navigate = route => () => {
@@ -29,20 +37,30 @@ export default class Topbar extends Component {
     const {t} = this.props
 
     return (
-      <div>
-        <div styleName="header">{t('AppTitle')}</div>
-        <div>
-          <div>
-            {navbar.map((nav, index) =>
-              <div key={index} styleName="nav-link">
-                <span style={{cursor: 'pointer', color: 'crimson'}} onClick={this.navigate(`${nav.link}`)}>
-                  {t(`nav.${nav.title}`)}
-                </span>
-              </div>
-            ) }
+      <nav className="column row">
+        <div className="top-bar">
+          <div className="top-bar-right">
+            <img src={logoSrc} alt={t('nav.logoAlt')} id="logo" />
+          </div>
+
+          <div className="top-bar-left" style={{position: 'relative'}}>
+            <div className="nav_icon_container" data-responsive-toggle="top_nav" data-hide-for="medium">
+              <button type="button" data-toggle="top_nav"><img src={navIconSrc} alt="" /></button>
+            </div>
+            <ul className="vertical dropdown medium-horizontal menu" id="top_nav" data-dropdown-menu>
+              {navbar.map((nav, index) =>
+                <li key={index}><a onClick={this.navigate(`${nav.link}`)}>{t(`nav.${nav.title}`)}</a></li>
+              ) }
+              <li>
+                <a href="#"><img src={userSrc} alt="" />{t('nav.fakeUser')}</a>
+                <ul className="menu">
+                  <li><a onClick={this.navigate('logout')}>{t('nav.logout')}</a></li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     )
   }
 }
--- src/common/translations/il.js
+++ src/common/translations/il.js
@@ -2,13 +2,19 @@ export default {
   page404: 'מצטערים, הקישור שגוי',
   loading: 'טוען...',
   AppTitle: 'מכרזים',
-  loginPage: {  
+  loginPage: {
     usernameLabel: 'שם משתמש',
     passwordLabel: 'סיסמה',
     loginBtnTitle: 'כניסה',
     loginError: 'פרטי ההזדהות שהזנת שגויים, אנא נסה שנית.'
   },
   nav: {
+    logoAlt: 'לוגו מכרזים',
+    subscriptions: 'מסלולים',
+    contactus: 'צור קשר',
+    publish: 'פרסם מכרז',
+    fakeUser: 'ורד קמין',
+    logout: 'התנתק',
     test: 'טסט',
     search: 'חיפוש'
   }
