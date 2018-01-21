9d33797e8b6ee437ca437ebc38abac7bf539fc53
Ori Shuchami <ori_s@ifat.com>
2018-01-21 13:48:23 +0200

    home categories css fix

--- src/components/Home/Items/CatItem.js
+++ src/components/Home/Items/CatItem.js
@@ -5,10 +5,10 @@ import styles from '../Home.scss'
 
 
 const CatItem = ({count, subSubjectID, catName}) => {
-  //const url = `#/category/${count}/${subSubjectID}/${catName}`  
+  //const url = `#/category/${count}/${subSubjectID}/${catName}`
   const url = `#/results/publishDate/[{"ID":${subSubjectID},"Name":"${catName}","ResType":"subsubject"}]/[]`
   return <div className="column column-block">
-    <a href={url} target="_blank" styleName="main_cat">
+    <a href={url} target="_blank" className="main_cat">
       <span styleName="cat_num">{count}</span>
       <h3 styleName="cat_name">{catName}</h3>
       <img src={getSrc(subSubjectID)} alt="" styleName="cat_icon" />
