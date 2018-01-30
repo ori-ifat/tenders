f0c2e4764d51fd7966ffc5f6ad5ad4e1a99d20c3
Ori Shuchami <ori_s@ifat.com>
2018-01-30 10:47:53 +0200

    home catItem link fix

--- src/components/Home/Items/CatItem.js
+++ src/components/Home/Items/CatItem.js
@@ -8,7 +8,7 @@ const CatItem = ({count, subSubjectID, catName}) => {
   //const url = `#/category/${count}/${subSubjectID}/${catName}`
   const url = `#/results/publishDate/[{"ID":${subSubjectID},"Name":"${encodeURIComponent(catName)}","ResType":"subsubject"}]/[]`
   return <div styleName="blockitem" className="column column-block">
-    <a href={url} target="_blank" className="main_cat">
+    <a href={url} className="main_cat">
       <span styleName="cat_num">{count}</span>
       <h3 styleName="cat_name">{catName}</h3>
       <img src={getSrc(subSubjectID)} alt="" styleName="cat_icon" />
